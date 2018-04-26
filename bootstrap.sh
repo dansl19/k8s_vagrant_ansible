51a2102bf1112f3a89a88f1df75cccc523c2ea00
Dan <danslutzky@gmail.com>
2018-04-26 17:20:53 +0200

    add new file

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y
+sudo sudo apt-get install -y python3-pip
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
